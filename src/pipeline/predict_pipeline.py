511bdbc6c2e7f8ed3ca4d9c76663440b59171b3a
OguzhanYapici <106562234+OguzhanYapici@users.noreply.github.com>
2024-11-01 17:23:11 +0000

    pipeline başlandı

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,10 @@
+import sys 
+import pandas as pd 
+from src.exception import CustomException 
+from src.utils import load_object 
+
+import os 
+
+class PredictPipeline:
+    def __init__(self):
+        pass # Başlangıç methodunda herhangi özellik tanımlıyoruz.
